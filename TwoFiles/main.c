342143691696d57e705ec3dd50c26ba96f6a0b3b
Danbug <you@example.comyou@example.com>
2022-09-24 00:40:26 +0300

    Fixed f and g misplacement

--- TwoFiles/main.c
+++ TwoFiles/main.c
@@ -8,6 +8,6 @@ int main(int argc, char const *argv[])
     double input = 0;
     printf("Enter x : ");
     scanf("%lf",&input);
-    printf("g(%.3lf) = %lf\nf(%.3lf) = %lf \n",input, f(input), input, g(input));
+    printf("f(%.3lf) = %lf\ng(%.3lf) = %lf \n",input, f(input), input, g(input));
     return 0;
 }
